7055ace0155df4f5cd347122aacc2ce16a872bee
popabogdannnn <62438990+popabogdannnn@users.noreply.github.com>
2021-03-22 18:11:40 +0200

    Am adaugat blocare cursorului in ecran

--- BetarStarcraft/Assets/RTS/GameService.cs
+++ BetarStarcraft/Assets/RTS/GameService.cs
@@ -7,13 +7,27 @@ namespace RTS {
     public static class GameService {
         //orice membru si metoda va trebui sa fie statica acum
 
-        public static float senzitivity = 20;
+        public static float senzitivity = 2;
 
         //dimensiunea de unde incepe camera sa se miste
         //de la marginea ecranului
-        public static int ScrollDim { get { return 10; }}
-        public static int RotateSpeed { get { return 100; }}
-        public static int RotateDim { get { return 15; }}
+
+        //CONSTANTE
+        public static int SCROLL_DIM { get { return 10; }}
+        public static int ROTATE_SPEED { get { return 100; }}
+        public static int ROTATE_DIM { get { return 15; }}
+
+        public static int MAX_FOV { get {return 125;} }
+        public static int MIN_FOV { get {return 2; } }
+        public static int MAX_ORTOGRAPHIC_SIZE { get {return 20;} }
+        public static int MIN_ORTOGRAPHIC_SIZE { get {return 1;} }
+        public static int DELTA_FOV {get {return 2;}}
+        public static float DELTA_ORTOGRAPHIC_SIZE { get {return 0.5f; } }
+        public static float MIN_CAMERA_HEIGHT { get { return 10; } }
+        public static float MAX_CAMERA_HEIGHT { get { return 40; } }
+
+        //END_CONSTANTE
+
 
         public static float GetSenzitivity() {
             return senzitivity;
@@ -23,8 +37,7 @@ namespace RTS {
             senzitivity = korean;
         }
 
-        public static float MinCameraHeight { get { return 10; } }
-        public static float MaxCameraHeight { get { return 40; } }
+        
 
     }
 }
